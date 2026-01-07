14693ac6b087daef593e32b3c2765bb4125f51ef
sanburnn <dimas00758@gmail.com>
2026-01-07 18:16:51 +0700

    qwen use

--- api.py
+++ api.py
@@ -17,6 +17,7 @@ app = FastAPI(
 # Load model ONCE (important)
 # -----------------------------
 tokenizer, model = load_model("qwen-1.5b")
+# tokenizer, model = load_model("llama-1b") # waiting for approval Llama
 
 # -----------------------------
 # Request / Response schemas
